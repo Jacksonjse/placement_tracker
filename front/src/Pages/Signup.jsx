de47cec7e7261ba4ea79c922907ec2e1ccd389ac
Jackson Jesse <100487502+Jacksonjse@users.noreply.github.com>
2025-10-31 05:18:44 +0530

    Update Signup.jsx

--- front/src/Pages/Signup.jsx
+++ front/src/Pages/Signup.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post('https://placement-tracker-back.onrender.com', { email, password });
       setToken(res.data.token);
       setUser(res.data.user);
       navigate('/dashboard');
