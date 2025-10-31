77f5a0d4d8f273053550d02f7eaf66eb54e78371
Jackson Jesse <100487502+Jacksonjse@users.noreply.github.com>
2025-10-31 05:42:12 +0530

    Update Dashboard.jsx

--- front/src/components/Dashboard.jsx
+++ front/src/components/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
 
   const handleDelete = async (id) => {
     if (user.role === 'admin') {
-      await axios.delete(`https://placement-tracker-back.onrender.com/${id}`, {
+      await axios.delete('https://placement-tracker-back.onrender.com/${id}', {
         headers: { Authorization: `Bearer ${getToken()}` },
       });
       setStudents(students.filter((s) => s._id !== id));
